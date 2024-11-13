e61f4042609abae33baffc0a8d937697372ae495
Sedenu <aloayecharles@gmail.com>
2024-11-13 07:58:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,10 @@ app.get('/', async (req, res) => {
 // Index Page Post
 app.post('/', async (req, res) => {
 
-    const API_URL = 'https://cleanuri.com/api/v1/shorten';
-        const response = await axios.get(API_URL, req.body);
-        const result = JSON.parse(response.data);
     try {
         const API_URL = 'https://cleanuri.com/api/v1/shorten';
         const response = await axios.get(API_URL, req.body);
-        const result = JSON.parsee(response.data);
+        const result = JSON.parse(response.data);
 
         res.render('page/index', { data: result, action: 'yes' });
 
